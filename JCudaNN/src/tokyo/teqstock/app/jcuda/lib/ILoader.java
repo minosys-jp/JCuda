a05b52d57ea22fc201749afbc8414be601bce66d
minosys-jp <minoru@minosys.com>
2017-11-02 20:19:24 +0900

    added ILoader.java

---
+++ JCudaNN/src/tokyo/teqstock/app/jcuda/lib/ILoader.java
@@ -0,0 +1,19 @@
+package tokyo.teqstock.app.jcuda.lib;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import jcuda.driver.CUdeviceptr;
+
+/**
+ * Loader インタフェース
+ * @author minoru
+ *
+ */
+public interface ILoader {
+	public void loader(BufferedInputStream bis) throws IOException;
+	public int getQuantity();
+	public float[] getContent(int k);
+	public void setContentDev();
+	public CUdeviceptr expandDevPtr(int[] samples);
+	public float[][] getContent();
+}
